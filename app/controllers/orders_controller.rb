64b1f3473af119eb3f01713839b03e6158b50715
Timilehin Emmanuel <Harriemannie@gmail.com>
2020-08-07 16:06:26 +0100

    create order model and controller

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,22 @@
+class OrdersController < ApplicationController
+  def create
+    bike = Bike.find(params[:bike_id])
+    order  = Order.create!(bike: bike, bike_sku: bike.sku, amount: bike.price, state: 'pending', user: current_user)
+
+    session = Stripe::Checkout::Session.create(
+      payment_method_types: ['card'],
+      line_items: [{
+        name: bike.sku,
+        images: [bike.photo],
+        amount: bike.price_cents,
+        currency: 'eur',
+        quantity: 1
+      }],
+      success_url: order_url(order),
+      cancel_url: order_url(order)
+    )
+
+    order.update(checkout_session_id: session.id)
+    redirect_to new_order_payment_path(order)
+  end
+end
