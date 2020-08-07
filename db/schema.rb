78181f8af391896ce80906e91c86f0536278f39f
Timilehin Emmanuel <Harriemannie@gmail.com>
2020-08-07 14:52:22 +0100

    add price to bikes and run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_07_112355) do
+ActiveRecord::Schema.define(version: 2020_08_07_134845) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2020_08_07_112355) do
     t.bigint "owner_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "price_cents", default: 0, null: false
     t.index ["owner_id"], name: "index_bikes_on_owner_id"
   end
 
