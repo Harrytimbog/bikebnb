eb74898c103cfe3163bd94aa3316630482fdedc1
Timilehin Emmanuel <Harriemannie@gmail.com>
2020-08-08 15:43:34 +0100

    .

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,4 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :bike
   validates :content, :rating, presence: true
-  has_many :reviews, dependent: :destroy
-  has_many :reviews, dependent: :destroy
 end
